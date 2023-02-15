0ae50e08e322a62c7191d501302ea723060f15ba
Francin Vincent <francin.vinc@gmail.com>
2023-02-15 22:18:55 +0530

    chore: paste code from Ex2 to new folder Ex3

---
+++ Ex3/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import Calculator from "@/components/Calculator.vue";
+import CalcLog from "@/components/CalcLog.vue";
+</script>
+
+<template>
+  <header>
+    <img
+      alt="Calculator logo"
+      class="logo"
+      src="./assets/logo.png"
+      width="125"
+      height="125"
+    />
+    <h1>Kalkulator</h1>
+    <p>En enkel kalkulator med design inspirasjon fra Apple.</p>
+  </header>
+
+  <main>
+    <Calculator />
+    <CalcLog />
+  </main>
+</template>
---
+++ Ex3/src/components/CalcButton.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+export default {
+  props: {
+    label: String,
+    action: Function,
+    style: String,
+  },
+};
+</script>
+
+<template>
+  <button @click="action" :class="style" class="calc-btn">{{ label }}</button>
+</template>
