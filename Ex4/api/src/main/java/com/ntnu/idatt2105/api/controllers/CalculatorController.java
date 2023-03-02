447a54aef373ad129524c3c7bd881545727f0a1c
Francin Vincent <francin.vinc@gmail.com>
2023-03-02 22:46:27 +0100

    chore: update cross origin

--- Ex4/api/src/main/java/com/ntnu/idatt2105/api/controllers/CalculatorController.java
+++ Ex4/api/src/main/java/com/ntnu/idatt2105/api/controllers/CalculatorController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://127.0.0.1:5173")
+@CrossOrigin(origins = "https://idatt2105-ex4.vercel.app")
 public class CalculatorController {
 
     @Autowired
